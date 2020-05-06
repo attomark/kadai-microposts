6b9baa18ec58675c753227aba499809bc001127b
attom <atten.atom@gmail.com>
2020-05-06 09:23:54 +0000

    favorites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,6 +38,13 @@ class UsersController < ApplicationController
     @followers = @user.followers.page(params[:page])
     counts(@user)
   end
+  
+  def likes
+    @user = User.find(params[:id])
+    @microposts = @user.favorite_microposts.order(id: :desc).page(params[:page])
+    counts(@user)
+    render :show
+  end
 
   private
 
